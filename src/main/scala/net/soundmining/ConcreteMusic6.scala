e44af0ced654dfafc4fc737399e9faa98f1d677f
Daniel Ståhl <oe.daniel.stahl@gmail.com>
2021-07-15 22:46:21 +0200

    Recapitulation

--- src/main/scala/net/soundmining/ConcreteMusic6.scala
+++ src/main/scala/net/soundmining/ConcreteMusic6.scala
@@ -30,6 +30,303 @@ object ConcreteMusic6 {
     soundPlays.init
   }
 
+  def recapitulation(): Unit = {
+    val absoluteTimes = Seq(0.228, 0.503, 0.864)
+    val ratio = (absoluteTimes(1) - absoluteTimes.head) / (absoluteTimes(2) - absoluteTimes.head) // 0.43238993710691825
+    val invertedRatio = 1 - ratio // 0.5676100628930818
+    val longTime = invertedRatio * 10 // 5.6761006289308185
+    val shortTime = ratio * 10 // 4.323899371069182
+
+    val times = Melody.absolute(0, Seq(
+      longTime, longTime, longTime, longTime, longTime * invertedRatio,
+      shortTime, shortTime, shortTime, shortTime * ratio,
+      longTime, longTime, longTime * invertedRatio,
+      shortTime, shortTime, shortTime * ratio,
+      longTime, longTime))
+
+    println(s"Times $times")
+
+    client.resetClock
+
+    val lowRate = 469.726 / 1156.29
+    val lowLowRate = lowRate * lowRate
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate, 1.0)
+      .pan(0).play(times.head, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.6).play(times.head + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.6).play(times.head + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate, 1.0)
+      .pan(0).play(times(1), 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 0.6)
+      .pan(0).play(times(1) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 1.0)
+      .sine(469.726 * lowRate)
+      .pan(0.9).play(times(1) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 1.0)
+      .sine(469.726 * lowLowRate)
+      .pan(-0.9).play(times(1) + 0.1, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate, 1.0)
+      .pan(0).play(times(2), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.6).play(times(2) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.6).play(times(2) + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate, 1.0)
+      .pan(0).play(times(3), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.6).play(times(3) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.6).play(times(3) + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate, 1.0)
+      .pan(0).play(times(4), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.6).play(times(4) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.6).play(times(4) + 0.02, 0)
+
+
+    val lowHighRate = 1103.37 / 2766.47
+    val lowLowHighRate = lowHighRate * lowHighRate
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(5), 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate + 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(0.6).play(times(5) + 0.01, 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate - 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(-0.6).play(times(5) + 0.02, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(6), 0)
+
+    soundPlays.mono(MORTAR_HIT_1)
+      .playMono(lowHighRate, 0.6)
+      .pan(-0.5).play(times(6) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_HIT_1)
+      .playMono(lowHighRate, 1.0)
+      .sine(1103.37 * lowHighRate)
+      .pan(0.5).play(times(6) + 0.1, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(7), 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate + 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(0.6).play(times(7) + 0.01, 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate - 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(-0.6).play(times(7) + 0.02, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(8), 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate + 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(0.6).play(times(8) + 0.01, 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate - 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(-0.6).play(times(8) + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate, 1.0)
+      .pan(0).play(times(9), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.7).play(times(9) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.7).play(times(9) + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate, 1.0)
+      .pan(0).play(times(10), 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 1.6)
+      .ring(1156.29 * lowRate)
+      .pan(0).play(times(10) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 2.0)
+      .sine(469.726 * lowRate)
+      .ring(1156.29 * lowRate)
+      .pan(0.9).play(times(10) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 2.0)
+      .sine(469.726 * lowLowRate)
+      .ring(1156.29 * lowRate)
+      .pan(-0.9).play(times(10) + 0.1, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate, 1.0)
+      .pan(0).play(times(11), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.7).play(times(11) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.7).play(times(11) + 0.02, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(12), 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate + 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(0.7).play(times(12) + 0.01, 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate - 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(-0.7).play(times(12) + 0.02, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(13), 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate + 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(0.7).play(times(13) + 0.01, 0)
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate - 0.001, 2.0)
+      .lowPass(1103.37 * lowLowHighRate)
+      .pan(-0.7).play(times(13) + 0.02, 0)
+
+
+    soundPlays.mono(SMALL_BOWL_1)
+      .playMono(lowHighRate, 1.0)
+      .pan(0).play(times(14), 0)
+
+    val highRate = 1156.29 / 469.726
+    val highRate2 = 2766.47 / 1103.37
+
+    soundPlays.mono(MORTAR_HIT_1)
+      .playMono(lowHighRate, 1.6)
+      .ring(1103.37 * highRate2)
+      .lowPass(1103.37 * highRate)
+      .pan(-0.5).play(times(14) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_HIT_1)
+      .playMono(lowHighRate, 2.0)
+      .sine(1103.37 * lowHighRate)
+      .ring(1103.37 * highRate2)
+      .lowPass(1103.37 * highRate)
+      .pan(0.5).play(times(14) + 0.1, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate, 1.0)
+      .pan(0).play(times(15), 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate + 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(0.8).play(times(15) + 0.01, 0)
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowRate - 0.001, 2.0)
+      .lowPass(469.726 * lowLowRate)
+      .pan(-0.8).play(times(15) + 0.02, 0)
+
+
+    soundPlays.mono(BIG_BOWL_2)
+      .playMono(lowLowRate, 1.0)
+      .pan(0).play(times(16), 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 0.6)
+      .pan(0).play(times(16) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 1.0)
+      .sine(469.726 * lowRate)
+      .pan(0.9).play(times(16) + 0.1, 0)
+
+    soundPlays.mono(MORTAR_RUMBLE_1)
+      .playMono(1, 1.0)
+      .sine(469.726 * lowLowRate)
+      .pan(-0.9).play(times(16) + 0.1, 0)
+  }
+
   def development2(): Unit = {
     client.resetClock
 
